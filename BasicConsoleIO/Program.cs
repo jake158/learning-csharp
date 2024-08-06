a7697db420ea2e9862c60b3bcd4dc8efd2f8ed4f
freeram <kulikovpavel2801@gmail.com>
2024-08-06 15:10:49 -0600

    Add basic console I/O

---
+++ BasicConsoleIO/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("Basic Console I/O");
+GetUserData();
+
+
+static void GetUserData()
+{
+    Console.Write("Enter your name: ");
+    string userName = Console.ReadLine();
+
+    ConsoleColor prevColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine("Hello {0}!", userName);
+
+    Console.ForegroundColor = prevColor;
+}
