9fcffb261f4ad96b89867468a094140f6e64fab0
freeram <kulikovpavel2801@gmail.com>
2024-08-06 15:17:05 -0600

    Add FormatNumericalData

--- BasicConsoleIO/Program.cs
+++ BasicConsoleIO/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine("Basic Console I/O");
 GetUserData();
+FormatNumericalData();
 
 
 static void GetUserData()
@@ -15,4 +16,19 @@ static void GetUserData()
     Console.WriteLine("Hello {0}!", userName);
 
     Console.ForegroundColor = prevColor;
+}
+
+static void FormatNumericalData()
+{
+    Console.WriteLine("The value 99999 in various formats:");
+    Console.WriteLine("c format: {0:c}", 99999);
+    Console.WriteLine("d9 format: {0:d9}", 99999);
+    Console.WriteLine("f3 format: {0:f3}", 99999);
+    Console.WriteLine("n format: {0:n}", 99999);
+    // Notice that upper- or lowercasing for hex
+    // determines if letters are upper- or lowercase.
+    Console.WriteLine("E format: {0:E}", 99999);
+    Console.WriteLine("e format: {0:e}", 99999);
+    Console.WriteLine("X format: {0:X}", 99999);
+    Console.WriteLine("x format: {0:x}", 99999);
 }
